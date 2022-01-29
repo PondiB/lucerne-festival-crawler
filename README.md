9d3ad23f0512c107a3b9f77350c15fd59b32fd61
Brian  Pondi <brian.pondi@targomo.com>
2022-01-29 11:33:21 +0100

    :art: saving to db implementations

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# musical-events-crawler
-Python scripts to web scrawl music events and save them up in a Postgres DB
+# musical events crawler
+Python scripts to scrawl music events and save them up in a Postgres DB
--- music_events_pipeline.py
+++ music_events_pipeline.py
@@ -1,14 +1,23 @@
 
+from distutils.debug import DEBUG
 import requests
 import csv
 from bs4 import BeautifulSoup
 import pandas as pd
 from sqlalchemy import create_engine
+from typing import List
+import logging
+from time import time
+
+logging.basicConfig(level=logging.INFO)
+
+__author__  = "Brian Pondi"
+__status__  = "Development"
 
 
-class MusicalEventsCrawler:
+class MusicalEventsPipeline:
     """
-    This class crawls the Lucerne festival in Switzerland and saves the data in a Postgres Database
+    This class crawls the Lucerne festival events scheduled for 2022 in Switzerland and saves the data in a Postgres Database.
     """
 
     def __init__(self) -> None:
@@ -20,9 +29,9 @@ class MusicalEventsCrawler:
         soup = BeautifulSoup(web_source, 'lxml')
         return soup
 
-    def _get_data_from_soup(self) -> list:
+    def _get_data_from_soup(self) -> List[dict]:
         """
-        Preprocess all the data from soup and return as list
+        Preprocess all the needed data from soup and return as list of dictionaries.
         """
         data = []
         soup = self._getsoup()
@@ -65,10 +74,12 @@ class MusicalEventsCrawler:
                 "image_url": image
                 }
                 data.append(data_dict)
-            #print(data)
         return data
 
     def save_to_csv(self, csv_file_name :str) -> None:
+        """
+        This method to save data into a csv is not a must I mainly used it to just validate things work.
+        """
         data = self._get_data_from_soup()
         keys = data[0].keys()
         with open(csv_file_name, 'w', newline='') as output_file:
@@ -76,10 +87,44 @@ class MusicalEventsCrawler:
             dict_writer.writeheader()
             dict_writer.writerows(data)
 
+    def _data_to_pandas_df(self) -> pd.DataFrame:
+        """
+        Convert data list of dicts to pandas dataframe.
+        """
+        data = self._get_data_from_soup()
+        df = pd.DataFrame(data)
+        return df
+
+    def _convert_datestring_to_date(self) -> pd.DataFrame:
+        """
+        The date field comes in as a string, it's ideal to have it as a timestamp.
+        """
+        df = self._data_to_pandas_df()
+        df['date'] = pd.to_datetime(df['date'])
+        return df
+
+    def save_to_postgres(self) -> None:
+        """
+        Saves Lucerne Festival events into a postgresql DB.
+        """
+        engine = create_engine('postgresql://root:root@localhost:5440/music_events')
+        df = self._convert_datestring_to_date()
+        logging.info(pd.io.sql.get_schema(df, name='ch_lucerne_festival', con=engine))
+        df.head(n=0).to_sql(name='ch_lucerne_festival', con=engine, if_exists='replace')
+        df.to_sql(name='ch_lucerne_festival', con=engine, if_exists='append')
+        logging.info('Finished inserting the data to Postgres')
+
 
 def main():
-    music_events = MusicalEventsCrawler()
+    start_time = time()
+
+    music_events = MusicalEventsPipeline()
     music_events.save_to_csv('musical_events_ch.csv')
+    music_events.save_to_postgres()
+
+    end_time = time()
+    time_taken = end_time - start_time
+    logging.info(f'The whole process took {time_taken} seconds')
 
 
 if __name__ == '__main__':
