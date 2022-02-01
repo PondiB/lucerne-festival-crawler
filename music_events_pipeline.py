f7e5c321bee9e0b842b3cb82eaf5d8ee5a049d51
Brian  Pondi <brian.pondi@targomo.com>
2022-02-01 21:23:14 +0100

    :fire: Refactored to reduce cognitive complexity, officially closed

--- music_events_pipeline.py
+++ music_events_pipeline.py
@@ -1,15 +1,16 @@
-import requests
-import csv
-from bs4 import BeautifulSoup
-import pandas as pd
-from sqlalchemy import create_engine
-from typing import List, Dict
-import logging
-from time import time
 import os
+import csv
+import requests
+import logging
+import pandas as pd
+from time import time
+from bs4 import BeautifulSoup
 from datetime import datetime
+from typing import List, Dict
+from sqlalchemy import create_engine
+
 import warnings
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 
 logging.basicConfig(filename='pipeline.log', level=logging.INFO)
 
@@ -27,7 +28,7 @@ class MusicalEventsPipeline:
         self.year = year
         logging.info(datetime.today().strftime('%Y-%m-%d-%H:%M:%S'))
         
-    def _getsoup(self):
+    def _getsoup(self) -> BeautifulSoup:
         """
         Returns soup of the website to be crawled on.
         """
@@ -36,54 +37,100 @@ class MusicalEventsPipeline:
         logging.info('Soup is ready')
         return soup
     
-    def _none_checker(self, field):
+    def _soup_none_checker(self, field: BeautifulSoup) -> str:
         """
         Checker that returns None for soup values are empty and returns text data if soup values have content.
         """
         return None if field is None else field.get_text().strip()
 
-    def _get_data_from_soup(self) -> List[Dict]:
+    def _get_date_details(self, event: BeautifulSoup) -> str:
+        """
+        Return full date from event i.e. date.month.year.
+        """
+        full_date = ''
+        date = event.find('p', class_='date')
+        date = self._soup_none_checker(date)
+        month = event.find('p', class_='month-number')
+        month = self._soup_none_checker(month)
+        if date is not None and month is not None:
+            full_date = date + month + self.year
+        return full_date
+
+    def _get_day_time_details(self, event: BeautifulSoup) -> List:
+        """
+        Return time and day data  as a list from event i.e. [time, day]
+        """
+        day_time = event.find('p', class_='day-time')
+        time = ''
+        day = ''
+        if day_time is not None:
+            time = day_time.find( 'span',class_ ='time')
+            time = self._soup_none_checker(time)
+            day = day_time.find( 'span',class_ ='day')
+            day = self._soup_none_checker(day)
+        return [time, day]
+
+    def _get_image_details(self, event: BeautifulSoup) -> str:
+        """
+        Return image url from the event.
+        """
+        image = event.find('div', class_='image')
+        if self._soup_none_checker(image)is not None:
+            image = str(image)
+            image = image.split("(", 1)[1].split(")")[0]
+        return image
+
+    def _get_artist_details(self, event: BeautifulSoup) -> str:
+        """
+        Return a list of artists name from the event.
+        """
+        artists = event.find('p', class_='title')
+        artists = self._soup_none_checker(artists)
+        if artists is not None:
+            artists = artists.split('|')
+            artists = ",".join(artists)
+        return artists
+
+    def _get_location_details(self, event: BeautifulSoup) -> str:
+        """
+        Return the venue of the event.
+        """
+        location = event.find('p', class_='location')
+        location = self._soup_none_checker(location)
+        return location
+
+    def _get_title_details(self, event: BeautifulSoup) -> str:
+        """
+        Return the title of the event.
+        """
+        title = event.find('p', class_='surtitle')
+        title = self._soup_none_checker(title)
+        return title
+
+
+    def _get_all_needed_data_from_soup(self) -> List[Dict]:
         """
         Preprocess all the needed data from soup and return as list of dictionaries.
         """
         data = []
         soup = self._getsoup()
         for event in soup.find_all('div', class_='entry'):
-            # Date
-            full_date = ''
-            date = event.find('p', class_='date')
-            date = self._none_checker(date)
-            month = event.find('p', class_='month-number')
-            month = self._none_checker(month)
-            if date is not None and month is not None:
-                full_date = date + month + self.year
-            #Event Day-Time
-            day_time = event.find('p', class_='day-time')
-            #Event Time & Event day as separate values
-            time = ''
-            day = ''
-            if day_time is not None:
-                time = day_time.find( 'span',class_ ='time')
-                time = self._none_checker(time)
-                day = day_time.find( 'span',class_ ='day')
-                day = self._none_checker(day)
+            # Full Date
+            full_date = self._get_date_details(event)
+            # Event Day-Time
+            day_time = self._get_day_time_details(event)
+            # Event Time & Event day as separate values
+            time = day_time[0]
+            day = day_time[1]
             # Location
-            location = event.find('p', class_='location')
-            location = self._none_checker(location)
+            location = self._get_location_details(event)
             # Title
-            title = event.find('p', class_='surtitle')
-            title = self._none_checker(title)
+            title = self._get_title_details(event)
             # image
-            image = event.find('div', class_='image')
-            if self._none_checker(image)is not None:
-                image = str(image)
-                image = image.split("(", 1)[1].split(")")[0]
-            # artists
-            artists = event.find('p', class_='title')
-            artists = self._none_checker(artists)
-            if artists is not None:
-                artists = artists.split('|')
-            #Add all datasets that are not none to dict
+            image = self._get_image_details(event)
+            # Artists
+            artists =  self._get_artist_details(event)
+            # Add all datasets that are not none to dict
             if title is not None:
                 data_dict = {
                 "date": full_date,
@@ -102,19 +149,19 @@ class MusicalEventsPipeline:
         """
         This method to save data into a csv is not a must I mainly used it to just validate things work.
         """
-        data = self._get_data_from_soup()
+        data = self._get_all_needed_data_from_soup()
         keys = data[0].keys()
         with open(csv_file_name, 'w', newline='') as output_file:
             dict_writer = csv.DictWriter(output_file, keys)
             dict_writer.writeheader()
             dict_writer.writerows(data)
-        logging.info('saved the data to csv')
+            logging.info('saved the data to csv')
 
     def _data_to_pandas_df(self) -> pd.DataFrame:
         """
         Convert data list of dicts to pandas dataframe.
         """
-        data = self._get_data_from_soup()
+        data = self._get_all_needed_data_from_soup()
         data_frame = pd.DataFrame(data)
         return data_frame
 
@@ -147,11 +194,11 @@ def main():
     password = os.environ['PG_PASSWORD']
     host = os.environ['PG_HOST']
     port = os.environ['PG_PORT']
-    db = os.environ['PG_DB_NAME']
+    db =  os.environ['PG_DB_NAME']
     table_name = os.environ['PG_TABLE_NAME']
     url = os.environ['WEB_URL']
     year= os.environ['YEAR']
-    
+
     start_time = time()
 
     music_events = MusicalEventsPipeline(url, year)
@@ -163,7 +210,7 @@ def main():
     time_taken = end_time - start_time
     logging.info(f'The whole process took {time_taken} seconds')
 
-    logging.info("--------------------------------------------------------------")
+    logging.info("==========================The End=============================")
     logging.info("--------------------------------------------------------------")
 
 
